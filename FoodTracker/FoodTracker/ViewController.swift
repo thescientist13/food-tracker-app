5a2341fbfd632b5fcb76642fbe89cd4af3d28595
Owen Buckley <owen@analogstudios.net>
2016-10-08 18:30:40 -0400

    project init

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Owen Buckley on 10/8/16.
+//  Copyright © 2016 The Greenhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
